063e5ff4fd33215febce80a28b05edbf183744ea
dp00 <dlrp00@yahoo.com>
2019-06-06 17:32:23 -0400

    Update and rename ListsV1.py to 08. ListsV1.py

--- 08. ListsV1.py
+++ 08. ListsV1.py
@@ -1,8 +1,11 @@
+#
 # Lists
+# Slide 26
+#
 
 students = ["Mary", "Ann", "Liz", "Sarah"]
 print(students[3])
 
-
+# Assignment
 colors= ["Red","Orange","Yellow","Green","Blue","Indigo","Violet"]
-
+print(colors[2])

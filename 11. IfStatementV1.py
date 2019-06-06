d6a3ff0b4d26a0b9caf7b67d39b689191879ca41
dp00 <dlrp00@yahoo.com>
2019-06-06 17:37:47 -0400

    Update 11. IfStatementV1.py

--- 11. IfStatementV1.py
+++ 11. IfStatementV1.py
@@ -1,12 +1,12 @@
 #
 # If Statements
-# Slide 30
+# Slide 30 and 31
 #
 name = "Katie"
 if name == "Katie":
     print("Hi Katie!")
 
-
+# Slide 31
 if name == "Katie":
     print("Hi Katie!")
 else:

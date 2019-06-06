0666ba1f18908b02d34e7a32582c2423a5107163
dp00 <dlrp00@yahoo.com>
2019-06-06 17:45:28 -0400

    Add files via upload

---
+++ 13. Puzzle 2 v1.py
@@ -0,0 +1,8 @@
+#
+# Puzzle 2
+# Slide 34
+#
+
+food=["ice cream","pizza","strawberries","chocolate", "lasagna"]
+for x in range(0,len(food)):
+  print(x, food[x])
---
+++ 14. Puzzle 2 v2.py
@@ -0,0 +1,9 @@
+#
+# Puzzle 2
+# Slide 34
+#
+
+food=["ice cream","pizza","strawberries","chocolate", "lasagna"]
+index=0
+for item in food:
+  print(index,item)

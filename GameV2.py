6b96a8afefe5796ed3e1ae4961d1e5dd2d36691a
dp00 <dlrp00@yahoo.com>
2019-06-06 07:40:59 -0400

    Add files via upload

--- GameV2.py
+++ GameV2.py
@@ -3,7 +3,7 @@ from random import randint
 secret_number = randint(1, 10)
 
 while True:
-    guess = input(“Pick a number between 1 and 10? ")
+    guess = input("Pick a number between 1 and 10? ")
 
     if secret_number == guess:
         print("Yay! You got it.")
